237afd2d0fd4dccb58f1133cf591b36427a4f904
XorTroll <33005497+XorTroll@users.noreply.github.com>
2018-04-04 15:29:39 +0200

    Add files via upload

---
+++ Libraries/nitroJS/include/nitroJS.h
@@ -0,0 +1,14 @@
+/*
+	 /-------------\
+	/\/\ nitroJS /\/\  : dukTape (C or C++ JS interpreter) adaptation for libnds
+	\---------------/
+	
+	dukTape interpreter: http://duktape.org/
+	Port made by XorRTroll
+	Version 0.1 / beta 1
+	
+	Version info: just dukTape added, no extra libs
+
+*/
+
+#include "duktape.h"
